fc46110a7cb575d2d647c6dbbcf0a402d0e9cc14
Charitha <168915906+Charitha1121@users.noreply.github.com>
2025-09-02 21:44:11 +0530

    Delete src/components/Loader.jsx

--- src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-function Loader() {
-  return (
-    <div className="flex items-center justify-center mt-4">
-      <div className="w-12 h-12 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
-    </div>
-  );
-}
-
-export default Loader;
